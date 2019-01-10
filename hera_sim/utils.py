4fa653aafac716284b19c5ee896d306b40bf3448
Steven Murray <steven.murray@curtin.edu.au>
2019-01-10 14:30:22 -0700

    allow baseline lengths to be vectors

--- hera_sim/utils.py
+++ hera_sim/utils.py
@@ -1,9 +1,51 @@
 """ Utility module """
 
-import numpy as np
-from scipy.interpolate import RectBivariateSpline
 import aipy
-from . import noise
+import numpy as np
+
+
+def _get_bl_len_vec(bl_len_ns):
+    """
+    Converts a baseline length in a variety of formats to a standard
+    length-3 vector.
+
+    Args:
+        bl_len_ns (scalar or array_like): the baseline length in nanosec (i.e.
+            1e9 * metres / c). If scalar, interpreted as E-W length, if len(2),
+            interpreted as EW and NS length, otherwise the full [EW, NS, Z]
+            length. Unspecified dimensions are assumed to be zero.
+
+    Returns:
+        length-3 array: The full [EW, NS, Z] baseline vector.
+    """
+    if np.isscalar(bl_len_ns):
+        bl_len_ns = np.array([bl_len_ns, 0, 0])
+    elif len(bl_len_ns) < 3:
+        # make a length-3 array
+        bl_len_ns = np.pad(
+            bl_len_ns, pad_width=3 - len(bl_len_ns),
+            mode='constant'
+        )
+
+    return bl_len_ns
+
+
+def get_bl_len_magnitude(bl_len_ns):
+    """
+    Get the magnitude of the length of the given baseline.
+
+    Args:
+        bl_len_ns (scalar or array_like): the baseline length in nanosec (i.e.
+            1e9 * metres / c). If scalar, interpreted as E-W length, if len(2),
+            interpreted as EW and NS length, otherwise the full [EW, NS, Z]
+            length. Unspecified dimensions are assumed to be zero.
+
+    Returns:
+        float: The magnitude of the baseline length.
+    """
+    bl_len_ns = _get_bl_len_vec(bl_len_ns)
+    return np.sqrt(np.sum(bl_len_ns ** 2))
+
 
 def rough_delay_filter(noise, fqs, bl_len_ns):
     """
@@ -13,17 +55,23 @@ def rough_delay_filter(noise, fqs, bl_len_ns):
     Args:
         noise : 1D or 2D ndarray, filtered along last axis
         fqs : 1D frequency array, [GHz]
-        bl_len_ns : baseline length, [nanosec]
+        bl_len_ns (scalar or array_like): the baseline length in nanosec (i.e.
+            1e9 * metres / c). If scalar, interpreted as E-W length, if len(2),
+            interpreted as EW and NS length, otherwise the full [EW, NS, Z]
+            length. Unspecified dimensions are assumed to be zero.
 
     Returns:
         filt_noise : delay-filtered noise
     """
-    delays = np.fft.fftfreq(fqs.size, fqs[1]-fqs[0])
+    bl_len_ns = get_bl_len_magnitude(bl_len_ns)
+
+    delays = np.fft.fftfreq(fqs.size, fqs[1] - fqs[0])
     _noise = np.fft.fft(noise)
-    delay_filter = np.exp(-delays**2 / (2*bl_len_ns**2))
-    delay_filter.shape = (1,) * (_noise.ndim-1) + (-1,)
+    delay_filter = np.exp(-delays ** 2 / (2 * bl_len_ns ** 2))
+    delay_filter.shape = (1,) * (_noise.ndim - 1) + (-1,)
     filt_noise = np.fft.ifft(_noise * delay_filter)
     return filt_noise
+
 
 def calc_max_fringe_rate(fqs, bl_len_ns):
     """
@@ -32,13 +80,34 @@ def calc_max_fringe_rate(fqs, bl_len_ns):
 
     Args:
         fqs : frequency array [GHz]
-        bl_len_ns : projected East-West baseline length [ns]
+        bl_len_ns (scalar or array_like): the baseline length in nanosec (i.e.
+            1e9 * metres / c). If scalar, interpreted as E-W length, if len(2),
+            interpreted as EW and NS length, otherwise the full [EW, NS, Z]
+            length. Unspecified dimensions are assumed to be zero.
+
     Returns:
         fr_max : fringe rate [lambda / sec]
     """
-    bl_wavelen = fqs * bl_len_ns
-    fr_max = 2*np.pi/aipy.const.sidereal_day * bl_wavelen
-    return fr_max
+    # If bl_len_ns is a scalar, interpret as E-W baseline, otherwise if
+    # len(2) list/array, interpret as EW, NS lengths, otherwise if len(3), as
+    # [EW, NS, Z].
+
+    if np.isscalar(bl_len_ns):
+        bl_len_ns = np.array([bl_len_ns, 0, 0])
+    elif len(bl_len_ns) < 3:
+        # make a length-3 array
+        bl_len_ns = np.pad(
+            bl_len_ns, pad_width=3 - len(bl_len_ns),
+            mode='constant'
+        )
+
+    earth_rotation = np.array([0, 2 * np.pi / aipy.const.sidereal_day, 0])
+
+    return fqs * np.cross(bl_len_ns, earth_rotation)[-1]
+    # bl_wavelen = fqs * bl_len_ns
+    # fr_max = 2*np.pi/aipy.const.sidereal_day * bl_wavelen
+    # return fr_max
+
 
 def rough_fringe_filter(noise, lsts, fqs, bl_len_ns, fr_width=None):
     """
@@ -48,7 +117,10 @@ def rough_fringe_filter(noise, lsts, fqs, bl_len_ns, fr_width=None):
     Args:
         noise : 1D or 2D ndarray, filtered along last axis
         fqs : 1D frequency array, [GHz]
-        bl_len_ns : baseline length, [nanosec]
+        bl_len_ns (scalar or array_like): the baseline length in nanosec (i.e.
+            1e9 * metres / c). If scalar, interpreted as E-W length, if len(2),
+            interpreted as EW and NS length, otherwise the full [EW, NS, Z]
+            length. Unspecified dimensions are assumed to be zero.
         fr_width : half-width of a Gaussian FR filter in [1/sec]
             to apply. If None filter is a flat-top FR filter.
             Can be a float or an array of size fqs.
@@ -56,27 +128,27 @@ def rough_fringe_filter(noise, lsts, fqs, bl_len_ns, fr_width=None):
     Returns:
         filt_noise : fringe-rate-filtered noise
     """
-    times = lsts / (2*np.pi) * aipy.const.sidereal_day
-    fringe_rates = np.fft.fftfreq(times.size, times[1]-times[0])
-    fringe_rates.shape = (-1,) + (1,) * (noise.ndim-1)
+    times = lsts / (2 * np.pi) * aipy.const.sidereal_day
+    fringe_rates = np.fft.fftfreq(times.size, times[1] - times[0])
+    fringe_rates.shape = (-1,) + (1,) * (noise.ndim - 1)
     _noise = np.fft.fft(noise, axis=-2)
     fr_max = calc_max_fringe_rate(fqs, bl_len_ns)
-    fr_max.shape = (1,) * (noise.ndim-1) + (-1,)
+    fr_max.shape = (1,) * (noise.ndim - 1) + (-1,)
 
     if fr_width is None:
         # use a top-hat filter with width set by maximum fr
         fng_filter = np.where(np.abs(fringe_rates) < fr_max, 1., 0)
     else:
         # use a gaussian centered at max fr
-        fng_filter = np.exp(-0.5 * ((fringe_rates-fr_max)/fr_width)**2)
+        fng_filter = np.exp(-0.5 * ((fringe_rates - fr_max) / fr_width) ** 2)
 
     filt_noise = np.fft.ifft(_noise * fng_filter, axis=-2)
 
     return filt_noise, fng_filter, fringe_rates
-    
+
+
 def compute_ha(lsts, ra):
     ha = lsts - ra
-    ha = np.where(ha > np.pi, ha-2*np.pi, ha)
-    ha = np.where(ha < -np.pi, ha+2*np.pi, ha)
+    ha = np.where(ha > np.pi, ha - 2 * np.pi, ha)
+    ha = np.where(ha < -np.pi, ha + 2 * np.pi, ha)
     return ha
-
