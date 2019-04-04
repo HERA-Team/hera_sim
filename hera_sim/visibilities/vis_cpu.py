dc165ffe9486f3528a3c0053d4e3cb4e15d8dbf0
Steven Murray <steven.murray@curtin.edu.au>
2019-04-04 08:15:37 -0700

    begin healvis wrapping

---
+++ hera_sim/visibilities/vis_cpu.py
@@ -0,0 +1,180 @@
+"""
+``vis_cpu`` visibility simulator.
+
+This is a fast, simple visibility simulator that
+"""
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+
+from . import conversions
+from .simulators import VisibilitySimulator
+
+
+class VisCPU(VisibilitySimulator):
+    _point_source_ability = False
+
+    def __init__(self, bm_pix=100, real_dtype=np.float32,
+                 complex_dtype=np.complex64, **kwargs):
+        """
+        Fast visibility simulator on the CPU.
+
+        Args:
+            bm_pix (int, optional): the number of pixels along a side in the
+                beam map when converted to (l,m).
+            real_dtype: a valid numpy dtype
+            complex_dtype: a valid numpy dtype
+            **kwargs:
+                All arguments of :class:`VisibilitySimulator`.
+
+        """
+        self._real_dtype = real_dtype
+        self._complex_dtype = complex_dtype
+        self.bm_pix = bm_pix
+
+        super(VisCPU, self).__init__(**kwargs)
+
+        # Convert some of our arguments to forms more simple for vis_cpu
+        self.antpos = self.antpos.astype(self._real_dtype)
+
+    def get_beam_lm(self):
+        """
+        Obtain the beam pattern in (l,m) co-ordinates for each beam.
+
+        Returns:
+              3D array, shape[NANT, BM_PIX, BM_PIX]: the beam pattern in (l,m)
+                  for each antenna.
+
+        Note:
+            Due to using the verbatim :func:`vis_cpu` function, the beam cube
+            must have an entry for each antenna, which is a bit of a waste of
+            memory in some cases. If this is changed in the future, this
+            method can be modified to only return one matrix for each beam.
+
+        """
+        return np.array([
+            conversions.beam_healpix_to_lm(
+                self.beams[self.beam_ids[i]], self.bm_pix
+            ) if self.beam_ids[i] >= 0 else np.ones((self.bm_pix, self.bm_pix))
+            for i in range(self.n_ant)
+        ])
+
+    def get_crd_eq(self):
+        """Calculate the equatorial co-ordinates of the healpix sky pixels."""
+        return conversions.healpix_to_crd_eq(self.sky_intensity).astype(self._real_dtype)
+
+    def get_eq2tops(self):
+        """
+        Calculate the set of 3x3 transformation matrices converting equatorial
+        coords to topocentric at each LST.
+        """
+        return conversions.eq2top_m(
+            self.lsts.astype(self._real_dtype),
+            (self.latitude * np.ones_like(self.lsts)).astype(self._real_dtype)
+        ).astype(self._real_dtype)
+
+    def simulate(self):
+        """
+        Runs the cpu_vis algorithm.
+
+        Returns:
+            array_like, shape(NTIMES, NANTS, NANTS): visibilities
+
+        Notes:
+            This routine does not support negative intensity values on the sky.
+        """
+        return vis_cpu(
+            antpos=self.antpos.astype(self._real_dtype),
+            freq=self.freq,
+            eq2tops=self.get_eq2tops(),
+            crd_eq=self.get_crd_eq(),
+            I_sky=self.sky_intensity,
+            bm_cube=self.get_beam_lm(),
+            real_dtype=self._real_dtype,
+            complex_dtype=self._complex_dtype
+        )
+
+
+def vis_cpu(antpos, freq, eq2tops, crd_eq, I_sky, bm_cube, real_dtype=np.float32,
+            complex_dtype=np.complex64):
+    """
+    Calculate visibility from an input intensity map and beam model.
+
+    Provided as a standalone function
+
+    Args:
+        antpos (array_like, shape: (NANT, 3)): antenna position array.
+        freq (float): frequency to evaluate the visibilities at [GHz].
+        eq2tops (array_like, shape: (NTIMES, 3, 3)): Set of 3x3 transformation
+            matrices converting equatorial coordinates to topocentric at each
+            hour angle (and declination) in the dataset.
+        crd_eq (array_like, shape: (3, NPIX)):
+            equatorial coordinates of Healpix pixels.
+        I_sky (array_like, shape: (NPIX,)): intensity distribution on the sky,
+            stored as array of Healpix pixels.
+        bm_cube (array_like, shape: (NANT, BM_PIX, BM_PIX)):
+            beam maps for each antenna.
+        real_dtype (dtype, optional):
+            data type to use for real-valued arrays.
+        complex_dtype (dtype, optional):
+            data type to use for complex-valued arrays.
+
+    Returns:
+        array_like, shape(NTIMES, NANTS, NANTS): visibilities
+    """
+    nant, ncrd = antpos.shape
+    assert ncrd == 3, "antpos must have shape (NANTS, 3)"
+    ntimes, ncrd1, ncrd2 = eq2tops.shape
+    assert ncrd1 == 3 and ncrd2 == 3, "eq2tops must have shape (NTIMES, 3, 3)"
+    ncrd, npix = crd_eq.shape
+    assert ncrd == 3, "crd_eq must have shape (3, NPIX)"
+    assert I_sky.ndim == 1 and I_sky.shape[0] == npix, "I_sky must have shape (NPIX,)"
+    bm_pix = bm_cube.shape[-1]
+    assert bm_cube.shape == (
+        nant,
+        bm_pix,
+        bm_pix,
+    ), "bm_cube must have shape (NANTS, BM_PIX, BM_PIX)"
+
+    # Intensity distribution (sqrt) and antenna positions
+    Isqrt = np.sqrt(I_sky).astype(real_dtype)  # XXX does not support negative sky
+    antpos = antpos.astype(real_dtype)
+    ang_freq = 2 * np.pi * freq
+
+    # Empty arrays: beam pattern, visibilities, delays, complex voltages
+    A_s = np.empty((nant, npix), dtype=real_dtype)
+    vis = np.empty((ntimes, nant, nant), dtype=complex_dtype)
+    tau = np.empty((nant, npix), dtype=real_dtype)
+    v = np.empty((nant, npix), dtype=complex_dtype)
+    crd_eq = crd_eq.astype(real_dtype)
+
+    bm_pix_x = np.linspace(-1, 1, bm_pix)
+    bm_pix_y = np.linspace(-1, 1, bm_pix)
+
+    # Loop over time samples
+    for t, eq2top in enumerate(eq2tops.astype(real_dtype)):
+        tx, ty, tz = crd_top = np.dot(eq2top, crd_eq)
+        for i in range(nant):
+            # Linear interpolation of primary beam pattern
+            spline = RectBivariateSpline(bm_pix_y, bm_pix_x, bm_cube[i], kx=1, ky=1)
+            A_s[i] = spline(ty, tx, grid=False)
+        A_s = np.where(tz > 0, A_s, 0)
+
+        # Calculate delays
+        np.dot(antpos, crd_top, out=tau)
+        np.exp((1.0j * ang_freq) * tau, out=v)
+
+        # Complex voltages
+        v *= A_s * Isqrt
+
+        # Compute visibilities (upper triangle only)
+        for i in range(len(antpos)):
+            np.dot(v[i: i + 1].conj(), v[i:].T, out=vis[t, i: i + 1, i:])
+
+    # Conjugate visibilities
+    np.conj(vis, out=vis)
+
+    # Fill in whole visibility matrix from upper triangle
+    for i in range(nant):
+        vis[:, i + 1:, i] = vis[:, i, i + 1:].conj()
+
+    return vis
