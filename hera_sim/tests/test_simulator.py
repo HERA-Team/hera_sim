c19eb23d919d6f820847726b490089461dcb9eca
Bobby Pascua <r.pascua@berkeley.edu>
2019-07-22 15:07:11 -0700

    Edited test_simulator.py script to not break from create_sim() function (the new implementation does not allow for the keyword no_autos).

--- hera_sim/tests/test_simulator.py
+++ hera_sim/tests/test_simulator.py
@@ -16,7 +16,7 @@ from hera_sim.noise import thermal_noise, HERA_Tsky_mdl
 from hera_sim.simulate import Simulator
 
 
-def create_sim(autos=False):
+def create_sim():
     return Simulator(
         n_freq=10,
         n_times=20,
@@ -24,7 +24,6 @@ def create_sim(autos=False):
             0: (20.0, 20.0, 0),
             1: (50.0, 50.0, 0)
         },
-        no_autos=not autos,
     )
 
 
@@ -56,7 +55,8 @@ def create_sim(autos=False):
 def test_from_empty():
     sim = create_sim()
 
-    assert sim.data.data_array.shape == (20, 1, 10, 1)
+    Nbls = len(np.unique(sim.data.baseline_array))
+    assert sim.data.data_array.shape == (Nbls*20, 1, 10, 1)
     assert np.all(np.isclose(sim.data.data_array, 0))
 
 
