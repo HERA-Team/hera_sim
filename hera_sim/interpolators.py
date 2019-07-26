f76f73ca5c217f8ad741755daab2b76948c128d6
Bobby Pascua <r.pascua@berkeley.edu>
2019-07-25 19:58:36 -0700

    Filled out the docstring for the Tsky class and its __init__ method.

--- hera_sim/interpolators.py
+++ hera_sim/interpolators.py
@@ -8,7 +8,11 @@ from scipy.interpolate import RectBivariateSpline
 
 class Tsky:
     """
-    # TODO: fill in docstring
+    This class provides an interface for converting hooks to npz files into
+    interpolation objects. In particular, this class takes a hook to a npz
+    file and creates an interpolation object for a sky temperature model given
+    the frequencies and LSTs (specified in the npz file) at which the sky
+    temperature array is evaluated.
     """
     def __init__(self, datafile, pol='xx', **interp_kwargs):
         self.datafile = datafile
@@ -17,6 +21,49 @@ class Tsky:
         self._check_pol(pol)
         self.pol = pol
         self._interp_kwargs = interp_kwargs
+
+    """
+    Initialize the Tsky object from a hook to a npz file with the required
+    information.
+
+    Args:
+        datafile (str): hook to a npz file
+            the npz file must contain the following information:
+                array of sky temperature values in units of Kelvins
+                this must be accessible via the key 'tsky' and must
+                have shape=(NPOLS, NLSTS, NFREQS).
+
+                array of frequencies at which the tsky model is evaluated
+                in units of GHz. this should have shape=(NFREQS,) and
+                should be accessible via the key 'freqs'.
+
+                array of LSTs at which the tsky model is evaulated in
+                units of radians. this should have shape=(NLSTS,) and
+                should be accessible via the key 'lsts'.
+
+                dictionary of metadata describing the data stored in the npz
+                file. currently it only needs to contain an entry 'pols', 
+                which lists the polarizations such that their order agrees
+                with the ordering of arrays along the tsky axis-0. the user
+                may choose to also save the units of the frequency, lst, and
+                tsky arrays as strings in this dictionary. this dictionary
+                must be accessible via the key 'meta'.
+
+        pol (str, optional; default='xx'): string specifying which polarization
+            to create the interpolation object for. pol must be in the list of
+            polarizations saved in the metadata dictionary.
+
+        **interp_kwargs (dict, optional): dictionary of kwargs for creating
+            the interpolation object. these are used to create an instance of
+            the scipy.interpolate.RectBivariateSpline class.
+
+    Raises:
+        AssertionError: if any of the required npz keys are not found. these
+            are 'freqs', 'lsts', 'tsky', and 'meta'. See the contents of the
+            docstring relating to the datafile argument for a more detailed
+            discussion. An AssertionError is also raised if the shape of the
+            tsky array is not (NPOLS, NLSTS, NFREQS).
+    """
 
     def __call__(self, lsts, freqs):
         return self._interpolator(lsts, freqs)
