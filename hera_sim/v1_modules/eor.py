363fa2d413d80bc9ebcc32a625ca0cc4ce248c81
Bobby Pascua <r.pascua@berkeley.edu>
2019-12-02 11:16:13 -0800

    Filled in NoiselikeEoR initializor signature based on original noiselike_eor signature. Added placeholder __call__ method for NoiselikeEoR class.

--- hera_sim/v1_modules/eor.py
+++ hera_sim/v1_modules/eor.py
@@ -10,7 +10,15 @@ class EoR:
         super().__init__(**kwargs)
 
 class NoiselikeEoR(EoR):
-    def __init__(self, **kwargs):
+    def __init__(self, eor_amp=1e-5, 
+                 min_delay=None, max_delay=None, 
+                 fringe_filter_type="tophat",
+                 fringe_filter_kwargs={}, **kwargs):
         super().__init__(**kwargs)
+    # TODO: docstrings
+
+    def __call__(self, lsts, freqs, bl_vec, **kwargs):
+        pass
+
 
 noiselike_eor = NoiselikeEoR()
