522987b3042576abf34ea56cfefece38b3e47773
Bobby Pascua <r.pascua@berkeley.edu>
2019-12-02 11:49:47 -0800

    Transcribed noiselike_eor contents to NoiselikeEoR's __call__ method.

--- hera_sim/v1_modules/eor.py
+++ hera_sim/v1_modules/eor.py
@@ -3,6 +3,7 @@
 from .components import registry 
 from abc import abstractmethod
 from cached_property import cached_property
+from . import utils
 
 @registry
 class EoR:
@@ -18,7 +19,27 @@ class NoiselikeEoR(EoR):
     # TODO: docstrings
 
     def __call__(self, lsts, freqs, bl_vec, **kwargs):
-        pass
+        # make white noise in freq/time
+        # XXX: original says in frate/freq, not sure why
+        data = utils.gen_white_noise(size=(len(lsts), len(freqs)))
+
+        # scale data by EoR amplitude
+        data *= eor_amp
+
+        # apply delay filter; default does nothing
+        # XXX find out why bl_len_ns is hardcoded as 1e10
+        # XXX also find out why a tophat filter is hardcoded
+        data = utils.rough_delay_filter(data, freqs, 1e10, 
+                                        filter_type="tophat",
+                                        min_delay=min_delay,
+                                        max_delay=max_delay)
+
+        # apply fringe-rate filter
+        data = utils.rough_fringe_filter(data, lsts, freqs, bl_vec[0], 
+                                         filter_type=fringe_filter_type, 
+                                         **fringe_filter_kwargs)
+
+        return data
 
 
 noiselike_eor = NoiselikeEoR()
