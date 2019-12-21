d6385e56782b3a2238831999bdd836598c21b156
Bobby Pascua <r.pascua@berkeley.edu>
2019-12-20 19:26:48 -0800

    Added functionality to have registry subclasses automatically update their call docstrings with the init docstrings; this makes sure that all of the parameters (required and optional) are listed in the call method docstring.

--- hera_sim/components.py
+++ hera_sim/components.py
@@ -13,6 +13,7 @@ def registry(cls):
         def __init_subclass__(cls, is_abstract=False, 
                               is_multiplicative=False, **kwargs):
             super().__init_subclass__(**kwargs)
+            cls._update_call_docstring()
             cls.is_multiplicative = is_multiplicative
             if not is_abstract:
                 cls.__base__._models[cls.__name__] = cls
@@ -43,6 +44,41 @@ def registry(cls):
                 error_msg += ", ".join([key for key in kwargs 
                                         if key not in self.kwargs])
                 raise ValueError(error_msg)
+
+        def _update_call_docstring(cls):
+            init_docstring = cls.__init__.__doc__
+            call_docstring = cls.__call__.__doc__
+            if any(
+                ["Parameters" not in doc 
+                 for doc in (init_docstring, call_docstring)
+                ]
+            ):
+                return
+            init_params = cls._extract_param_section(init_docstring)
+            call_params = cls._extract_param_section(call_docstring)
+            full_params = call_params + init_params
+            cls.__call__.__doc__.replace(call_params, full_params)
+
+        @staticmethod
+        def _extract_param_section(docstring):
+            # make a regular expression to capture section headings
+            pattern = re.compile("[A-Za-z]+\n.*-+\n")
+            # get the section headings
+            section_headings = pattern.findall(docstring)
+            if not section_headings[0].lower().startswith("param"):
+                # TODO: make this error message a bit better
+                # or just make it a warning instead
+                raise SyntaxError(
+                    "Please ensure that the 'Parameters' section of "
+                    "the docstring comes first."
+                )
+            # get everything after the first heading
+            param_section = docstring.partition(section_headings[0])[-1]
+            # just return this if there are no more sections
+            if len(section_headings) == 1:
+                return param_section
+            # return everything before the next section
+            return param_section.partition(section_headings[1])[0]
 
     NewClass.__name__ = cls.__name__
     return NewClass
