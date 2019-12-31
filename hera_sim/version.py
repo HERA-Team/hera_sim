5ba1216072723290ae51418dfd0cff62c9fb1ccb
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-31 11:51:49 -0700

    adds a nasty try/except: pip installs future if not found in version.py

--- hera_sim/version.py
+++ hera_sim/version.py
@@ -2,7 +2,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
-from future import standard_library
+
+try:
+    from future import standard_library
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', '--user', 'future'])
+    from future import standard_library
+
 standard_library.install_aliases()
 from builtins import *
 from builtins import str
