066bb7348333bcf0fc8f8568b48fb76a10cfe896
Steven Murray <steven.murray@curtin.edu.au>
2020-01-10 15:22:29 -0700

    Separate tests

--- hera_sim/tests/test_vis.py
+++ hera_sim/tests/test_vis.py
@@ -1,19 +1,16 @@
 import unittest
 
-from hera_sim import vis
-from hera_sim.antpos import linear_array
-
+import healpy
+import healvis
 import numpy as np
 import pytest
-import warnings
-
 from astropy.units import sday
 from pyuvsim.analyticbeam import AnalyticBeam
-from hera_sim.visibilities import VisCPU, HealVis
-from hera_sim import io
-import healvis
 
-import healpy
+from hera_sim import io
+from hera_sim import vis
+from hera_sim.antpos import linear_array
+from hera_sim.visibilities import VisCPU, HealVis
 
 SIMULATORS = (HealVis, VisCPU)
 
@@ -34,6 +31,8 @@ def uvdata():
             0: (0, 0, 0),
         },
     )
+
+
 @pytest.fixture
 def uvdataJD():
     return io.empty_uvdata(
@@ -46,7 +45,8 @@ def uvdataJD():
         start_time=2456659
     )
 
-def test_healvis_beam(uvdata, tmpdir):
+
+def test_healvis_beam(uvdata):
     freqs = np.unique(uvdata.freq_array)
 
     # just anything
@@ -58,53 +58,55 @@ def test_healvis_beam(uvdata, tmpdir):
         sky_freqs=np.unique(uvdata.freq_array),
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     )
 
     assert len(hv.beams) == 1
     assert isinstance(hv.beams[0], healvis.beam_model.AnalyticBeam)
 
+
+def test_healvis_beam_obsparams(tmpdir):
     # Now try creating with an obsparam file
     direc = tmpdir.mkdir("test_healvis_beam")
 
     with open(direc.join("catalog.txt"), 'w') as fl:
         fl.write("""SOURCE_ID       RA_J2000 [deg]  Dec_J2000 [deg] Flux [Jy]       Frequency [Hz]
-HERATEST0       68.48535        -28.559917      1       100000000.0
-""")
+    HERATEST0       68.48535        -28.559917      1       100000000.0
+    """)
 
     with open(direc.join("telescope_config.yml"), 'w') as fl:
         fl.write("""
-beam_paths:
-    0 : 'uniform'
-telescope_location: (-30.72152777777791, 21.428305555555557, 1073.0000000093132)
-telescope_name: MWA        
-""")
+    beam_paths:
+        0 : 'uniform'
+    telescope_location: (-30.72152777777791, 21.428305555555557, 1073.0000000093132)
+    telescope_name: MWA        
+    """)
 
     with open(direc.join("layout.csv"), 'w') as fl:
         fl.write("""Name     Number   BeamID   E          N          U
 
-Tile061        40        0   -34.8010   -41.7365     1.5010
-Tile062        41        0   -28.0500   -28.7545     1.5060
-Tile063        42        0   -11.3650   -29.5795     1.5160
-Tile064        43        0    -9.0610   -20.7885     1.5160
-""")
+    Tile061        40        0   -34.8010   -41.7365     1.5010
+    Tile062        41        0   -28.0500   -28.7545     1.5060
+    Tile063        42        0   -11.3650   -29.5795     1.5160
+    Tile064        43        0    -9.0610   -20.7885     1.5160
+    """)
 
     with open(direc.join("obsparams.yml"), 'w') as fl:
         fl.write("""
-freq:
-  Nfreqs: 1
-  channel_width: 80000.0
-  start_freq: 100000000.0
-sources:
-  catalog: {0}/catalog.txt
-telescope:
-  array_layout: {0}/layout.csv
-  telescope_config_name: {0}/telescope_config.yml
-time:
-  Ntimes: 1
-  integration_time: 11.0
-  start_time: 2458098.38824015
-""".format(direc.strpath))
+    freq:
+      Nfreqs: 1
+      channel_width: 80000.0
+      start_freq: 100000000.0
+    sources:
+      catalog: {0}/catalog.txt
+    telescope:
+      array_layout: {0}/layout.csv
+      telescope_config_name: {0}/telescope_config.yml
+    time:
+      Ntimes: 1
+      integration_time: 11.0
+      start_time: 2458098.38824015
+    """.format(direc.strpath))
 
     hv = HealVis(obsparams=direc.join("obsparams.yml").strpath)
     beam = hv.beams[0]
@@ -112,6 +114,7 @@ time:
     print(type(beam))
     print(beam.__class__)
     assert isinstance(beam, healvis.beam_model.AnalyticBeam)
+
 
 def test_JD(uvdata, uvdataJD):
     freqs = np.unique(uvdata.freq_array)
@@ -125,7 +128,7 @@ def test_JD(uvdata, uvdataJD):
         sky_freqs=np.unique(uvdata.freq_array),
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     ).simulate()
 
     viscpu2 = VisCPU(
@@ -138,6 +141,7 @@ def test_JD(uvdata, uvdataJD):
 
     assert viscpu1.shape == viscpu2.shape
     assert not np.allclose(viscpu1, viscpu2, atol=0.1)
+
 
 @pytest.fixture
 def uvdata2():
@@ -169,7 +173,8 @@ def test_shapes(uvdata, simulator):
         sky_intensity=I_sky,
     )
 
-    assert v.simulate().shape == (NTIMES*len(uvdata.get_antpairs()), 1, NFREQ, 1) # len(uvdata.get_antpairs()) = number of baselines
+    assert v.simulate().shape == (NTIMES * len(uvdata.get_antpairs()), 1, NFREQ,
+                                  1)  # len(uvdata.get_antpairs()) = number of baselines
 
 
 @pytest.mark.parametrize(
@@ -218,6 +223,7 @@ def test_autocorr_flat_beam(uvdata, simulator):
     np.testing.assert_allclose(np.abs(v), np.mean(v), rtol=1e-5)
     np.testing.assert_almost_equal(np.abs(v), 0.5, 2)
 
+
 @pytest.mark.parametrize("simulator", SIMULATORS)
 def test_single_source_autocorr(uvdata, simulator):
     freqs = np.unique(uvdata.freq_array)
@@ -231,13 +237,14 @@ def test_single_source_autocorr(uvdata, simulator):
         sky_freqs=np.unique(uvdata.freq_array),
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4,
+        nside=2 ** 4,
     ).simulate()
 
     # Make sure the source is over the horizon half the time
     # (+/- 1 because of the discreteness of the times)
     # 1e-3 on either side to account for float inaccuracies.
-    assert -1e-3 + (NTIMES/2.0 - 1.0)/NTIMES <= np.round(np.abs(np.mean(v)), 3) <= (NTIMES/2.0 + 1.0)/NTIMES + 1e-3
+    assert -1e-3 + (NTIMES / 2.0 - 1.0) / NTIMES <= np.round(np.abs(np.mean(v)), 3) <= (
+                NTIMES / 2.0 + 1.0) / NTIMES + 1e-3
 
 
 @pytest.mark.parametrize("simulator", SIMULATORS)
@@ -253,12 +260,13 @@ def test_single_source_autocorr_past_horizon(uvdata, simulator):
         sky_freqs=np.unique(uvdata.freq_array),
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     ).simulate()
 
     assert np.abs(np.mean(v)) == 0
 
-def align_src_to_healpix(point_source_pos, point_source_flux, nside=2**4):
+
+def align_src_to_healpix(point_source_pos, point_source_flux, nside=2 ** 4):
     """Where the point sources will be placed when converted to healpix model
     
     Parameters
@@ -278,17 +286,18 @@ def align_src_to_healpix(point_source_pos, point_source_flux, nside=2**4):
     new_flux: ndarray
         Corresponding new flux values.       
     """
-    
+
     hmap = np.zeros((len(point_source_flux), healpy.nside2npix(nside)))
 
     # Get which pixel every point source lies in.
-    pix = healpy.ang2pix(nside, np.pi/2 - point_source_pos[:, 1], point_source_pos[:, 0])
+    pix = healpy.ang2pix(nside, np.pi / 2 - point_source_pos[:, 1], point_source_pos[:, 0])
 
     hmap[:, pix] += point_source_flux / healpy.nside2pixarea(nside)
     nside = healpy.get_nside(hmap[0])
     ra, dec = healpy.pix2ang(nside, np.arange(len(hmap[0])), lonlat=True)
     flux = hmap * healpy.nside2pixarea(nside)
-    return np.array([ra*np.pi/180, dec*np.pi/180]).T, flux
+    return np.array([ra * np.pi / 180, dec * np.pi / 180]).T, flux
+
 
 def test_comparison_zenith(uvdata2):
     freqs = np.unique(uvdata2.freq_array)
@@ -296,7 +305,7 @@ def test_comparison_zenith(uvdata2):
     # put a point source in
     point_source_pos = np.array([[0, uvdata2.telescope_location_lat_lon_alt[0]]])
     point_source_flux = np.array([[1.0]] * len(freqs))
-    
+
     # align to healpix center for direct comparision
     point_source_pos, point_source_flux = align_src_to_healpix(point_source_pos, point_source_flux)
 
@@ -305,7 +314,7 @@ def test_comparison_zenith(uvdata2):
         sky_freqs=freqs,
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     ).simulate()
 
     healvis = HealVis(
@@ -315,26 +324,27 @@ def test_comparison_zenith(uvdata2):
         point_source_pos=point_source_pos,
         nside=2 ** 4
     ).simulate()
-   
+
     assert viscpu.shape == healvis.shape
-    np.testing.assert_allclose(viscpu, healvis, rtol=0.05) 
+    np.testing.assert_allclose(viscpu, healvis, rtol=0.05)
+
 
 def test_comparision_horizon(uvdata2):
     freqs = np.unique(uvdata2.freq_array)
 
     # put a point source in
-    point_source_pos = np.array([[0, uvdata2.telescope_location_lat_lon_alt[0] + np.pi/2]])
+    point_source_pos = np.array([[0, uvdata2.telescope_location_lat_lon_alt[0] + np.pi / 2]])
     point_source_flux = np.array([[1.0]] * len(freqs))
 
     # align to healpix center for direct comparision
-    point_source_pos, point_source_flux = align_src_to_healpix(point_source_pos, point_source_flux)    
-    
+    point_source_pos, point_source_flux = align_src_to_healpix(point_source_pos, point_source_flux)
+
     viscpu = VisCPU(
         uvdata=uvdata2,
         sky_freqs=freqs,
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     ).simulate()
 
     healvis = HealVis(
@@ -344,27 +354,28 @@ def test_comparision_horizon(uvdata2):
         point_source_pos=point_source_pos,
         nside=2 ** 4
     ).simulate()
-   
+
     assert viscpu.shape == healvis.shape
     np.testing.assert_allclose(viscpu, healvis, rtol=0.05)
+
 
 def test_comparison_multiple(uvdata2):
     freqs = np.unique(uvdata2.freq_array)
 
     # put a point source in
-    point_source_pos = np.array([[0, uvdata2.telescope_location_lat_lon_alt[0] + np.pi/4],
+    point_source_pos = np.array([[0, uvdata2.telescope_location_lat_lon_alt[0] + np.pi / 4],
                                  [0, uvdata2.telescope_location_lat_lon_alt[0]]])
     point_source_flux = np.array([[1.0, 1.0]] * len(freqs))
 
     # align to healpix center for direct comparision
     point_source_pos, point_source_flux = align_src_to_healpix(point_source_pos, point_source_flux)
-    
+
     viscpu = VisCPU(
         uvdata=uvdata2,
         sky_freqs=freqs,
         point_source_flux=point_source_flux,
         point_source_pos=point_source_pos,
-        nside=2**4
+        nside=2 ** 4
     ).simulate()
 
     healvis = HealVis(
@@ -377,20 +388,21 @@ def test_comparison_multiple(uvdata2):
 
     assert viscpu.shape == healvis.shape
     np.testing.assert_allclose(viscpu, healvis, rtol=0.05)
-    
+
+
 def test_comparison_half(uvdata2):
     freqs = np.unique(uvdata2.freq_array)
     nbase = 4
-    nside = 2**nbase
+    nside = 2 ** nbase
 
     I_sky = create_uniform_sky(nbase=nbase)
 
-    vec = healpy.ang2vec(np.pi/2, 0)
+    vec = healpy.ang2vec(np.pi / 2, 0)
     # Zero out values within pi/2 of (theta=pi/2, phi=0)
-    ipix_disc = healpy.query_disc(nside=nside, vec=vec, radius=np.pi/2)
+    ipix_disc = healpy.query_disc(nside=nside, vec=vec, radius=np.pi / 2)
     for i in range(len(freqs)):
         I_sky[i][ipix_disc] = 0
-        
+
     viscpu = VisCPU(
         uvdata=uvdata2,
         sky_freqs=freqs,
@@ -404,23 +416,24 @@ def test_comparison_half(uvdata2):
         sky_intensity=I_sky,
         nside=nside
     ).simulate()
-    
+
     assert viscpu.shape == healvis.shape
     np.testing.assert_allclose(viscpu, healvis, rtol=0.05)
-    
+
+
 def test_comparision_airy(uvdata2):
     freqs = np.unique(uvdata2.freq_array)
     nbase = 4
-    nside = 2**nbase
+    nside = 2 ** nbase
 
     I_sky = create_uniform_sky(nbase=nbase)
 
-    vec = healpy.ang2vec(np.pi/2, 0)
+    vec = healpy.ang2vec(np.pi / 2, 0)
     # Zero out values within pi/2 of (theta=pi/2, phi=0)
-    ipix_disc = healpy.query_disc(nside=nside, vec=vec, radius=np.pi/2)
+    ipix_disc = healpy.query_disc(nside=nside, vec=vec, radius=np.pi / 2)
     for i in range(len(freqs)):
         I_sky[i][ipix_disc] = 0
-        
+
     viscpu = VisCPU(
         uvdata=uvdata2,
         sky_freqs=freqs,
@@ -439,6 +452,7 @@ def test_comparision_airy(uvdata2):
 
     assert viscpu.shape == healvis.shape
     np.testing.assert_allclose(viscpu, healvis, rtol=0.05)
+
 
 class TestSimRedData(unittest.TestCase):
 
@@ -484,7 +498,8 @@ class TestSimRedData(unittest.TestCase):
                 np.testing.assert_almost_equal(ans0yx, ans_yx, decimal=7)
                 np.testing.assert_almost_equal(ans0yy, ans_yy, decimal=7)
 
-        # Test that redundant baselines are redundant up to the gains in 4-pol minV mode (where Vxy = Vyx)
+        # Test that redundant baselines are redundant up to the gains in 4-pol minV mode (where
+        # Vxy = Vyx)
         reds = om.get_reds(antpos, pols=['xx', 'yy', 'xy', 'yX'], pol_mode='4pol_minV')
         gains, true_vis, data = vis.sim_red_data(reds)
         assert len(gains) == 2 * (5)
@@ -509,6 +524,6 @@ class TestSimRedData(unittest.TestCase):
                 np.testing.assert_almost_equal(ans0yx, ans_yx, decimal=7)
                 np.testing.assert_almost_equal(ans0yy, ans_yy, decimal=7)
 
-    
+
 if __name__ == "__main__":
     unittest.main()
