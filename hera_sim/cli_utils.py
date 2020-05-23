1027b18d96d72bc27bf690b90f5e5d4bad83c92e
Bobby Pascua <r.pascua@berkeley.edu>
2020-05-22 23:52:37 -0700

    Write function for extracting filing parameters from a configuration dictionary.

--- hera_sim/cli_utils.py
+++ hera_sim/cli_utils.py
@@ -2,3 +2,22 @@
 Module containing useful helper functions and argparsers for running 
 simulations with hera_sim via the command line.
 """
+import os
+from pathlib import Path
+
+def get_filing_params(config):
+    """Extract filing parameters from a configuration dictionary."""
+
+    filing_params = dict(
+        outdir=os.getcwd(),
+        outfile_name="hera_sim_simulation.uvh5",
+        output_format="uvh5",
+        clobber=False,
+    )
+    filing_params.update(config.get("filing", {}))
+    if filing_params["output_format"] not in ("miriad", "uvfits", "uvh5"):
+        raise ValueError(
+            "Output format not supported. Please use miriad, uvfits, or uvh5."
+        )
+
+    return filing_params
