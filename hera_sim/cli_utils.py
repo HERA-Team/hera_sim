8ec4dc0f2ecb22fcf92684c431fb8faefbc24a58
Bobby Pascua <r.pascua@berkeley.edu>
2020-06-01 11:08:45 -0700

    Write function for saving gains as a calfits file.

--- hera_sim/cli_utils.py
+++ hera_sim/cli_utils.py
@@ -5,6 +5,7 @@ simulations with hera_sim via the command line.
 import os
 from pathlib import Path
 from .defaults import SEASON_CONFIGS
+from .simulate import Simulator
 
 def get_filing_params(config):
     """Extract filing parameters from a configuration dictionary."""
@@ -39,3 +40,29 @@ def validate_config(config):
     freqs_ok, times_ok, array_ok = (True,) * 3
     if not all(freqs_ok, times_ok, array_ok):
         raise ValueError("Insufficient information to initialize simulation.")
+
+def write_calfits(
+    gains, 
+    filename, 
+    sim=None, 
+    freqs=None, 
+    times=None, 
+    clobber=False
+):
+    """Write gains to disk as a calfits file."""
+    from hera_cal.io import write_cal
+    if sim is not None:
+        if isinstance(sim, Simulator):
+            freqs = sim.freqs * 1e9
+            times = sim.times
+        else:
+            freqs = np.unique(sim.freq_array)
+            times = np.unique(sim.time_array)
+    else:
+        if freqs is None or times is None:
+            raise ValueError(
+                "If a simulation is not provided, then both frequencies and "
+                "times must be specified."
+            )
+
+    write_cal(filename, gains, freqs, times, overwrite=clobber, return_uvc=False)
