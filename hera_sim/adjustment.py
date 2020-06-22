507a56ccf873b9b0f4d606b36a9a62379fa76d29
Bobby Pascua <r.pascua@berkeley.edu>
2020-06-22 13:16:46 -0700

    Write interpolate_to_reference function in adjustment module.

--- hera_sim/adjustment.py
+++ hera_sim/adjustment.py
@@ -6,7 +6,7 @@ import pathlib
 
 import numpy as np
 from astropy import units
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, RectBivariateSpline
 from warnings import warn
 
 from pyuvdata import UVData
@@ -18,6 +18,7 @@ try:
     import hera_cal
 except ImportError:
     warn("hera_cal is not installed. Certain features will be unavailable.")
+
 
 def adjust_to_reference(
     target,
@@ -35,7 +36,7 @@ def adjust_to_reference(
 
     Parameters
     ----------
-    target: str, path-like object, or :class:`pyuvdata.UVData` instance
+    target: str, path-like object, or :class:`pyuvdata.UVData`-like object
         Target object to modify. If it is a path-like object, then it is
         loaded into a :class:`pyuvdata.UVData` object.
     reference: str, path-like object, or :class:`pyuvdata.UVData` instance
@@ -53,7 +54,7 @@ def adjust_to_reference(
         shift them in a way that produces the maximal overlap with the reference
         antenna positions.
     position_tolerance: float or array-like of float, optional
-        Tolerance for adjusting antenna positions, in meters. Default is one 
+        Tolerance for adjusting antenna positions, in meters. Default is one
         meter in x, y, and z. Specifying a single number will use the same value
         for each component; specifying a length-3 array of floats will use those
         values for the individual components, in ENU-coordinates.
@@ -108,29 +109,6 @@ def adjust_to_reference(
     """
     if verbose:
         print("Validating positional arguments...")
-    target_is_simulator = False
-    if not isinstance(target, UVData):
-        if isinstance(target, Simulator):
-            target_is_simulator = True
-            target_original = copy.deepcopy(target)
-            target = target.data
-            target_original.data = None # Memory concerns for later
-        else:
-            target_files = _listify(target)
-            _validate_file_list(target_files, "target")
-            target = UVData()
-            target.read(target_files)
-
-    if not isinstance(reference, UVData):
-        if isinstance(reference, Simulator):
-            reference_metadata = reference.data.copy(metadata_only=True)
-        else:
-            reference_files = _listify(reference)
-            _validate_file_list(reference_files, "reference")
-            reference_metadata = UVData()
-            reference_metadata.read(reference_files, read_data=False)
-    else:
-        reference_metadata = reference.copy(metadata_only=True)
 
     # Quickly check that the tolerance for the antenna adjustment is valid.
     # (Processing prior to antenna adjustment can be long, so do this early.)
@@ -148,6 +126,22 @@ def adjust_to_reference(
             "position_tolerance must be a real-valued scalar or length-3 array."
         )
 
+    # Check if the target object is a Simulator, but work with a UVData object.
+    target_is_simulator = isinstance(target, Simulator)
+    target = _to_uvdata(target)
+
+    # Pull the reference metadata.
+    if not isinstance(reference, UVData):
+        if isinstance(reference, Simulator):
+            reference_metadata = reference.data.copy(metadata_only=True)
+        else:
+            reference_files = _listify(reference)
+            _validate_file_list(reference_files, "reference")
+            reference_metadata = UVData()
+            reference_metadata.read(reference_files, read_data=False)
+    else:
+        reference_metadata = reference.copy(metadata_only=True)
+
     if verbose:
         if interpolate:
             print("Interpolating target data to reference data LSTs...")
@@ -158,8 +152,7 @@ def adjust_to_reference(
         target = interpolate_to_reference(target, reference_metadata)
     else:
         if not np.isclose(
-            target.integration_time.mean(), 
-            reference_metadata.integration_time.mean()
+            target.integration_time.mean(), reference_metadata.integration_time.mean()
         ):
             msg = "Target and reference integration times do not match. "
             msg += "This may result in discontinuities in the result."
@@ -188,10 +181,235 @@ def adjust_to_reference(
         target.conjugate_bls(conjugation_convention)
 
     if target_is_simulator:
-        target_original.data = target
-        return target_original
+        target = Simulator(data=target)
 
     return target
+
+
+def match_antennas(
+    target,
+    reference,
+    tol=1.0,
+    relabel_antennas=True,
+    use_reference_positions=False,
+    overwrite_telescope_metadata=False,
+):
+    pass
+
+
+def interpolate_to_reference(
+    target,
+    reference=None,
+    ref_times=None,
+    ref_lsts=None,
+    ref_freqs=None,
+    axis="time",
+    kind="cubic",
+):
+    """
+    Interpolate target visibilities to reference times/frequencies. Interpolation
+    may be along one axis or both. Interpolating data with a phase wrap or
+    interpolating data to a phase-wrapped set of LSTs is currently unsupported.
+
+    Parameters
+    ----------
+    target: :class:`pyuvdata.UVData` instance or :class:`Simulator` instance
+        Object containing the visibility data and metadata which is to be
+        interpolated to the reference LSTs and/or frequencies.
+    reference: :class:`pyuvdata.UVData` instance or :class:`Simulator` instance
+        Object containing reference metadata. Does not need to be provided if
+        metadata arrays are provided (``ref_times``, ``ref_lsts``, and/or
+        ``ref_freqs``, depending on which axis is used).
+    ref_times: array-like of float
+        Reference times corresponding to the LSTs the target will be interpolated
+        to, in units of JD. Must be provided if ``reference`` is not provided and
+        interpolation is along the time axis.
+    ref_lsts: array-like of float
+        Reference LSTs to interpolate the target data to, in units of radians.
+        Must be provided if ``reference`` is not provided and interpolation is
+        along the time axis.
+    ref_freqs: array-like of float
+        Reference frequencies to interpolate the target data to, in units of Hz.
+        Must be provided if ``reference`` is not provided and interpolation is
+        along frequency axis.
+    axis: str, optional
+        Axis to perform interpolation along. Must be one of the following: 'time',
+        'freq', or 'both'. Default is to interpolate along the time axis.
+    kind: str, optional
+        Order of the spline interpolator used. A cubic spline is used by default.
+
+    Returns
+    -------
+    modified_data: :class:`pyuvdata.UVData` instance or :class:`Simulator` instance
+        Data adjusted so that its times match the reference times which correspond
+        to overlapping LSTs. The type of object returned is the same as the type
+        of object passed.
+    """
+    # First, do a check of reference information
+    if reference is not None:
+        ref_time_to_lst_map = {
+            ref_time: ref_lst
+            for ref_time, ref_lst in zip(reference.time_array, reference.lst_array)
+        }
+        ref_times = np.array(list(ref_time_to_lst_map.keys()))
+        ref_lsts = np.array(list(ref_time_to_lst_map.values()))
+        ref_freqs = np.unique(reference.freq_array)
+    else:
+        if axis in ("time", "both") and (ref_lsts is None or ref_times is None):
+            raise ValueError(
+                "Time and LST reference information must be provided for "
+                "interpolation along time-axis."
+            )
+        if axis in ("freq", "both") and ref_freqs is None:
+            raise ValueError(
+                "Frequency reference information must be provided for "
+                "interpolation along frequency-axis."
+            )
+
+    target_is_simulator = isinstance(target, Simulator)
+    target = _to_uvdata(target)
+    target_time_to_lst_map = {
+        target_time: target_lst
+        for target_time, target_lst in zip(target.time_array, target.lst_array)
+    }
+    target_lsts = np.array(list(target_time_to_lst_map.values()))
+    target_freqs = np.unique(target.freq_array)
+
+    # TODO: figure out how to handle phase wraps
+    def iswrapped(lsts):
+        return np.any(lsts < lsts[0])
+
+    if iswrapped(target_lsts) or iswrapped(ref_lsts):
+        raise NotImplementedError(
+            "Either the target LSTs or the reference LSTs have a phase wrap. "
+            "This is currently not supported."
+        )
+
+    # Ensure reference parameters are a subset of target parameters.
+    if axis in ("time", "both"):
+        if np.any(np.logical_or(ref_lsts < target_lsts[0], ref_lsts > target_lsts[-1])):
+            warn("Reference LSTs not a subset of target LSTs; clipping.")
+            key = np.argwhere(
+                np.logical_and(target_lsts[0] <= ref_lsts, ref_lsts <= target_lsts[-1])
+            ).flatten()
+            ref_times = ref_times[key]
+            ref_lsts = ref_lsts[key]
+    if axis in ("freq", "both"):
+        if np.any(
+            np.logical_or(ref_freqs < target_freqs[0], ref_freqs > target_freqs[-1])
+        ):
+            warn("Reference frequencies not a subset of target frequencies; clipping.")
+            key = np.argwhere(
+                np.logical_and(
+                    target_freqs[0] <= ref_freqs, ref_freqs <= target_freqs[-1]
+                )
+            ).flatten()
+            ref_freqs = ref_freqs[key]
+
+    # Setup data/metadata objects that need to be non-trivially rewritten.
+    if axis in ("time", "both"):
+        new_Nblts = ref_times.size * target.Nbls
+        new_time_array = np.empty(new_Nblts, dtype=float)
+        new_lst_array = np.empty(new_Nblts, dtype=float)
+        new_ant_1_array = np.empty(new_Nblts, dtype=int)
+        new_ant_2_array = np.empty(new_Nblts, dtype=int)
+        new_baseline_array = np.empty(new_Nblts, dtype=int)
+        new_uvw_array = np.empty((new_Nblts, 3), dtype=float)
+        if axis == "both":
+            new_data_shape = (new_Nblts, 1, ref_freqs.size, target.Npols)
+        else:
+            new_data_shape = (new_Nblts, 1, target_freqs.size, target.Npols)
+        new_data = np.zeros(new_data_shape, dtype=np.complex)
+    else:
+        new_data_shape = (target.Nblts, 1, ref_freqs.size, target.Npols)
+
+    # Actually update metadata and interpolate the data.
+    for i, bl in enumerate(target.get_antpairs()):
+        if axis == "freq":
+            for pol_ind, pol in enumerate(target.polarization_array):
+                vis = target.get_data(bl + (pol,))
+                this_blt_slice = target._key2inds(bl + (pol,))[0]
+                re_spline = interp1d(target_freqs, vis.real, axis=1, kind=kind)
+                im_spline = interp1d(target_freqs, vis.imag, axis=1, kind=kind)
+                new_data[this_blt_slice, 0, :, pol_ind] = re_spline(
+                    ref_freqs
+                ) + 1j * im_spline(ref_freqs)
+            continue
+
+        # Preparation for updating metadata.
+        ant1, ant2 = bl
+        this_slice = slice(i, None, target.Nbls)
+        old_blt = target._key2inds(bl)[0][0]  # As a reference
+        this_uvw = target.uvw_array[old_blt]
+        this_baseline = target.baseline_array[old_blt]
+
+        # Now actually update the metadata.
+        new_ant_1_array[this_slice] = ant1
+        new_ant_2_array[this_slice] = ant2
+        new_baseline_array[this_slice] = this_baseline
+        new_uvw_array[this_slice] = this_uvw
+        new_time_array[this_slice] = ref_times
+        new_lst_array[this_slice] = ref_lsts
+
+        # Update the data.
+        for pol_ind, pol in enumerate(target.polarization_array):
+            vis = target.get_data(bl + (pol,))
+            if axis == "both":
+                re_spline = RectBivariateSpline(
+                    target_lsts, target_freqs, vis.real, kind=kind
+                )
+                im_spline = RectBivariateSpline(
+                    target_lsts, target_freqs, vis.imag, kind=kind
+                )
+                new_data[this_slice, 0, :, pol_ind] = re_spline(
+                    ref_lsts, ref_freqs
+                ) + 1j * im_spline(ref_lsts, ref_freqs)
+            else:
+                re_spline = interp1d(target_lsts, vis.real, axis=0, kind=kind)
+                im_spline = interp1d(target_lsts, vis.imag, axis=0, kind=kind)
+                new_data[this_slice, 0, :, pol_ind] = re_spline(
+                    ref_lsts
+                ) + 1j * im_spline(ref_lsts)
+
+    # Finally, update all of the metadata.
+    if axis in ("freq", "both"):
+        target.Nfreqs = ref_freqs.size
+    if axis in ("time", "both"):
+        target.Ntimes = ref_times.size
+        target.time_array = new_time_array
+        target.lst_array = new_lst_array
+        target.ant_1_array = new_ant_1_array
+        target.ant_2_array = new_ant_2_array
+        target.baseline_array = new_baseline_array
+        target.uvw_array = new_uvw_array
+        target.blt_order = None
+
+    # Now update the data-like attributes
+    target.flag_array = np.zeros(new_data.shape, dtype=bool)
+    target.nsample_array = np.ones(new_data.shape, dtype=float)
+    target.data_array = new_data
+    if target_is_simulator:
+        target = Simulator(data=target)
+
+    return target
+
+
+def rephase_to_reference(
+    target, reference=None, ref_times=None, ref_lsts=None,
+):
+    """
+    Rephase target data to match overlapping reference LSTs. This function
+    requires that ``hera_cal`` be installed.
+
+    Parameters
+    ----------
+    target: :class:`pyuvdata.UVData` instance or :class:`Simulator` instance
+        Object containing the visibility data and metadata which is to be
+        rephased to reference LSTs.
+    reference:
+    """
+    pass
+
 
 def _validate_file_list(file_list, name="file list"):
     """Ensure all entries in the file list are path-like objects."""
@@ -200,3 +418,25 @@ def _validate_file_list(file_list, name="file list"):
             f"{name} must be either a collection of path-like objects or a "
             "UVData object or Simulator object"
         )
+
+
+def _to_uvdata(sim):
+    """Convert input object to a :class:`pyuvdata.UVData` object."""
+    if isinstance(sim, UVData):
+        return sim
+    elif isinstance(sim, Simulator):
+        return sim.data
+    elif isinstance(sim, (str, pathlib.Path)):
+        uvd = UVData()
+        uvd.read(sim)
+        return uvd
+    else:
+        try:
+            _ = iter(sim)
+            if not all(os.path.exists(entry) for entry in sim):
+                raise TypeError
+            uvd = UVData()
+            uvd.read(sim)
+            return uvd
+        except TypeError:
+            raise TypeError("Input object could not be converted to UVData object.")
