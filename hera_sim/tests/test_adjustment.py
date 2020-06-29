97494eb227d5e4407b9023b337677601333078ad
Bobby Pascua <r.pascua@berkeley.edu>
2020-06-28 17:05:07 -0700

    Add helper function for comparing antenna position dictionaries and fix a bug in adjustment testing script.

--- hera_sim/tests/test_adjustment.py
+++ hera_sim/tests/test_adjustment.py
@@ -10,6 +10,12 @@ from hera_sim import adjustment
 from hera_sim import antpos
 from hera_sim import Simulator
 
+def antpos_equal(antpos_1, antpos_2, tol=0.1):
+    return all(
+        any(np.allclose(pos_1, pos_2, atol=tol) for pos_1 in antpos_1.values())
+        for pos_2 in antpos_2.values()
+    )
+
 def test_antenna_matching():
     # Simple test: reflected right-triangles should match exactly
     array_1 = {
@@ -25,11 +31,8 @@ def test_antenna_matching():
     }
 
     array_intersection = adjustment._get_array_intersection(array_1, array_2, tol=0)
-    assert all(
-        any(np.allclose(pos_1, pos_2) for pos_1 in array_2.values())
-        for pos_2 in array_intersection.values()
-    )
-
+    assert antpos_equal(array_intersection, array_2, 0)
+    
     # In general, rotated right-triangles should have at least two antennas in their
     # intersection.
     array_3 = {
@@ -41,23 +44,15 @@ def test_antenna_matching():
     assert len(array_intersection) == 2
 
     # A simple translation should just be undone
-    array_4 = {
-        ant: np.array(pos) - np.random.uniform(-1, 1, 3)
-        for ant, pos in array_1.items()
-    }
-    array_intersection = adjustment._get_array_intersection(array_1, array_4, tol=0)
-    assert all(
-        any(np.allclose(pos_1, pos_2) for pos_1 in array_4.values())
-        for pos_2 in array_intersection.values()
-    )
+    translation = np.random.uniform(-1, 1, 3)
+    array_4 = {ant: np.array(pos) - translation for ant, pos in array_1.items()}
+    array_intersection = adjustment._get_array_intersection(array_1, array_4, tol=0.1)
+    assert antpos_equal(array_intersection, array_4, 0.1)
 
     # A small hex array should be a subset of a larger hex array
     hex_array = antpos.HexArray(split_core=False, outriggers=0)
     hex_array_1 = hex_array(4)
     hex_array_2 = hex_array(3)
     array_intersection = adjustment._get_array_intersection(hex_array_1, hex_array_2)
-    assert all(
-        any(np.allclose(pos_1, pos_2) for pos_1 in hex_array_2.values())
-        for pos_2 in array_intersection.values()
-    )
+    assert antpos_equal(array_intersection, hex_array_2)
     assert len(array_intersection) == len(hex_array_2)
