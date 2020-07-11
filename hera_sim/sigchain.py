ce40627b5072a1d800d888f866978f49f8ebc068
Bobby Pascua <r.pascua@berkeley.edu>
2020-07-10 21:16:16 -0700

    Remove xtalk symmetry enforcement.
    Undo the change that enforced that crosstalk is symmetric in delay. Add
    an option in the xtalk spectrum class to symmetrize the spectrum (i.e.
    make the spectrum also appear at the negative of the delay range
    specified). Add a note regarding differences in the parameter jitter
    between the original crosstalk spectrum and the negative delay copy.

--- hera_sim/sigchain.py
+++ hera_sim/sigchain.py
@@ -264,10 +264,8 @@ class CrossCouplingCrosstalk(Crosstalk, Reflections):
             [1], amp, dly, phs, amp_jitter, dly_jitter
         )
 
-        # Make reflection coefficient; enforce symmetry in delay.
-        eps_1 = self.gen_reflection_coefficient(freqs, amp, dly, phs, conj=conj)
-        eps_2 = self.gen_reflection_coefficient(freqs, amp, -dly, phs, conj=conj)
-        eps = eps_1 + eps_2
+        # Make reflection coefficient.
+        eps = self.gen_reflection_coefficient(freqs, amp, dly, phs, conj=conj)
 
         # reshape if necessary
         if eps.ndim == 1:
@@ -288,6 +286,7 @@ class CrossCouplingSpectrum(Crosstalk):
         phs_range=(-np.pi, np.pi),
         amp_jitter=0,
         dly_jitter=0,
+        symmetrize=True,
     ):
         super().__init__(
             Ncopies=Ncopies,
@@ -296,6 +295,7 @@ class CrossCouplingSpectrum(Crosstalk):
             phs_range=phs_range,
             amp_jitter=amp_jitter,
             dly_jitter=dly_jitter,
+            symmetrize=symmetrize,
         )
 
     def __call__(self, freqs, autovis, **kwargs):
@@ -311,6 +311,7 @@ class CrossCouplingSpectrum(Crosstalk):
             phs_range,
             amp_jitter,
             dly_jitter,
+            symmetrize,
         ) = self._extract_kwarg_values(**kwargs)
 
         # Construct the arrays of amplitudes and delays.
@@ -329,6 +330,10 @@ class CrossCouplingSpectrum(Crosstalk):
             )
 
             crosstalk_spectrum += gen_xtalk(freqs, autovis)
+            if symmetrize:
+                # Note: this will have neither the same jitter realization nor
+                # the same phase as the first crosstalk spectrum.
+                crosstalk_spectrum += gen_xtalk(freqs, autovis, dly=-dly)
 
         return crosstalk_spectrum
 
