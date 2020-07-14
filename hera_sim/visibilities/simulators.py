Fix healpix catalogs unit conversion
`pyradiosky` no longer converts healpix catalogs to point-source catalogs. The current master of `hera_sim` (https://github.com/HERA-Team/hera_sim/commit/c2774efaa1e8d44775d643f2069e10b036e0ea75) assumes that conversion from healpix to point-source is being done in `pyradiosky`. This commit adds a try-except block to catch `UnitConversionError` that occurs when healpix catalogs are being read as point-source catalogs and correctly read in Stokes 'I'  as `sky_intensity`.

hera_sim/visibilities/simulators.py
@@ -102,8 +102,13 @@ class VisibilitySimulator(object):
                     catalog.at_frequencies(np.unique(self.uvdata.freq_array) * units.Hz)
                     point_source_pos = np.array([catalog.ra.rad, catalog.dec.rad]).T
 
-                    # This gets the 'I' component of the flux density
-                    point_source_flux = np.atleast_2d(catalog.stokes[0].to('Jy').value).T
+                    try:
+                        # This gets the 'I' component of the flux density
+                        point_source_flux = np.atleast_2d(catalog.stokes[0].to('Jy').value).T
+                    except UnitConversionError:
+                        # If the catalog is healpix, converting Stokes 'I' to 'Jy' will give
+                        # `UnitConversionError`. Then, we get the 'I' component as sky intensity.
+                        sky_intensity = np.atleast_2d(catalog.stokes[0].to('K').value).T
                 except KeyError:
                     # If 'catalog' was not defined in obsparams, that's fine. We assume
                     # the user has passed some sky model directly (we'll catch it later).