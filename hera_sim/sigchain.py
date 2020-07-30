Fix numpy dtype broadcasting error.
Some of the tests involving jittering reflection parameters were failing
on my local branch due to a type broadcasting error raised by numpy (it
didn't like mixing floats with integers). I modified the function
responsible for this potential error to ensure that jittered parameters
are always returned with np.float dtype.

hera_sim/sigchain.py
@@ -208,14 +208,16 @@ class Reflections(Gain, is_multiplicative=True):
 
         def broadcast_param(param, lower_bound, upper_bound, size):
             if param is None:
-                return stats.uniform.rvs(lower_bound, upper_bound, size)
+                params = stats.uniform.rvs(lower_bound, upper_bound, size)
             elif np.isscalar(param):
-                return np.ones(size) * param
+                params = np.ones(size) * param
             else:
                 if len(param) == size:
-                    return np.array(param)
+                    params = np.array(param)
                 else:
-                    return stats.uniform.rvs(*param, size)
+                    params = stats.uniform.rvs(*param, size)
+
+            return params.astype(np.float)
 
         # Transform parameters into arrays.
         amps = broadcast_param(amp, 0, 1, len(ants))