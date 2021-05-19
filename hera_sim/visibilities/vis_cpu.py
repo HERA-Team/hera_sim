9a21a0ae1113e82e3f528bfb0f96a3346d36a8ec
Steven Murray <steven.g.murray@asu.edu>
2021-05-19 12:20:49 -0700

    maint: remove vis_cpu function form package

--- hera_sim/visibilities/vis_cpu.py
+++ hera_sim/visibilities/vis_cpu.py
@@ -8,7 +8,7 @@ from . import conversions
 from .simulators import VisibilitySimulator
 
 from astropy.constants import c
-
+from vis_cpu import vis_cpu, vis_gpu, HAVE_GPU
 
 class VisCPU(VisibilitySimulator):
     """
@@ -42,16 +42,12 @@ class VisCPU(VisibilitySimulator):
             self._real_dtype = np.float64
             self._complex_dtype = np.complex128
 
-        if use_gpu:
-            try:
-                from hera_gpu.vis import vis_gpu
-                self._vis_cpu = vis_gpu
-            except ImportError:
-                raise ImportError(
-                    'GPU acceleration requires hera_gpu (`pip install hera_sim[gpu]`).'
-                )
-        else:
-            self._vis_cpu = vis_cpu
+        if use_gpu and not HAVE_GPU:
+            raise ImportError(
+                'GPU acceleration requires hera_gpu (`pip install hera_sim[gpu]`).'
+            )
+        
+        self._vis_cpu = vis_gpu if use_gpu else vis_cpu
         self.bm_pix = bm_pix
 
         super(VisCPU, self).__init__(**kwargs)
@@ -244,107 +240,3 @@ class VisCPU(VisibilitySimulator):
         if self.point_source_flux is not None:
             vis += self._simulate_points()
         return vis
-
-
-def vis_cpu(antpos, freq, eq2tops, crd_eq, I_sky, bm_cube,
-            precision=1):
-    """
-    Calculate visibility from an input intensity map and beam model.
-
-    Provided as a standalone function.
-
-    Parameters
-    ----------
-    antpos : array_like
-        Antenna position array. Shape=(NANT, 3).
-    freq : float
-        Frequency to evaluate the visibilities at [GHz].
-    eq2tops : array_like
-        Set of 3x3 transformation matrices converting equatorial
-        coordinates to topocentric at each
-        hour angle (and declination) in the dataset.
-        Shape=(NTIMES, 3, 3).
-    crd_eq : array_like
-        Equatorial coordinates of Healpix pixels, in Cartesian system.
-        Shape=(3, NPIX).
-    I_sky : array_like
-        Intensity distribution on the sky,
-        stored as array of Healpix pixels. Shape=(NPIX,).
-    bm_cube : array_like
-        Beam maps for each antenna. Shape=(NANT, BM_PIX, BM_PIX).
-    real_dtype {np.float32, np.float64}
-        Data type to use for real-valued arrays.
-    complex_dtype {np.complex64, np.complex128}
-        Data type to use for complex-valued arrays.
-
-    Returns
-    -------
-    array_like
-        Visibilities. Shape=(NTIMES, NANTS, NANTS).
-    """
-
-    assert precision in (1,2)
-    if precision == 1:
-        real_dtype=np.float32
-        complex_dtype=np.complex64
-    else:
-        real_dtype=np.float64
-        complex_dtype=np.complex128
-    nant, ncrd = antpos.shape
-    assert ncrd == 3, "antpos must have shape (NANTS, 3)."
-    ntimes, ncrd1, ncrd2 = eq2tops.shape
-    assert ncrd1 == 3 and ncrd2 == 3, "eq2tops must have shape (NTIMES, 3, 3)."
-    ncrd, npix = crd_eq.shape
-    assert ncrd == 3, "crd_eq must have shape (3, NPIX)."
-    assert I_sky.ndim == 1 and I_sky.shape[0] == npix, \
-        "I_sky must have shape (NPIX,)."
-    bm_pix = bm_cube.shape[-1]
-    assert bm_cube.shape == (
-        nant,
-        bm_pix,
-        bm_pix,
-    ), "bm_cube must have shape (NANTS, BM_PIX, BM_PIX)."
-
-    # Intensity distribution (sqrt) and antenna positions. Does not support
-    # negative sky.
-    Isqrt = np.sqrt(I_sky).astype(real_dtype)
-    antpos = antpos.astype(real_dtype)
-
-    ang_freq = 2 * np.pi * freq
-
-    # Empty arrays: beam pattern, visibilities, delays, complex voltages.
-    A_s = np.empty((nant, npix), dtype=real_dtype)
-    vis = np.empty((ntimes, nant, nant), dtype=complex_dtype)
-    tau = np.empty((nant, npix), dtype=real_dtype)
-    v = np.empty((nant, npix), dtype=complex_dtype)
-    crd_eq = crd_eq.astype(real_dtype)
-
-    bm_pix_x = np.linspace(-1, 1, bm_pix)
-    bm_pix_y = np.linspace(-1, 1, bm_pix)
-
-    # Loop over time samples.
-    for t, eq2top in enumerate(eq2tops.astype(real_dtype)):
-        tx, ty, tz = crd_top = np.dot(eq2top, crd_eq)
-
-        for i in range(nant):
-            # Linear interpolation of primary beam pattern.
-            spline = RectBivariateSpline(bm_pix_y, bm_pix_x, bm_cube[i], kx=1,
-                                         ky=1)
-            A_s[i] = spline(ty, tx, grid=False)
-
-        A_s = np.where(tz > 0, A_s, 0)
-
-        # Calculate delays, where TAU = (b * s) / c.
-        np.dot(antpos, crd_top, out=tau)
-        tau /= c.value
-
-        np.exp(1.0j * (ang_freq * tau), out=v)
-
-        # Complex voltages.
-        v *= A_s * Isqrt
-
-        # Compute visibilities (upper triangle only).
-        for i in range(len(antpos)):
-            np.dot(v[i: i + 1].conj(), v[i:].T, out=vis[t, i: i + 1, i:])
-
-    return vis
