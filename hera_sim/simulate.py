fix: silent bug when random seed left as None
Rather than choose a seed based on the current random state, a warning
is issued informing the user that the simulated effect might not be
exactly recoverable. The previous implementation caused the same random
seed to be used for every baseline, which is *not* intended. If we wish
to keep the behavior of "every simulated effect can be recovered later",
then support of providing None as a seed needs to be dropped and the
default setting needs to be changed to "initial".

hera_sim/simulate.py
@@ -254,9 +254,11 @@ class Simulator:
             model = model(**kwargs)
         self._sanity_check(model)  # Check for component ordering issues.
         self._antpairpol_cache[model_key] = []  # Initialize this model's cache.
-        if seed is None:
-            # Ensure we can recover the data later via ``get``
-            seed = int(np.random.get_state()[1][0])
+        if seed is None and add_vis:
+            warnings.warn(
+                "You have not specified how to seed the random state."
+                "This effect might not be exactly recoverable."
+            )
 
         # Simulate the effect by iterating over baselines and polarizations.
         data = self._iteratively_apply(
@@ -276,10 +278,9 @@ class Simulator:
                     if param not in kwargs and param in defaults():
                         kwargs[param] = defaults(param)
             self._update_history(model, **kwargs)
-            # Record the random state in case no seed was specified.
-            # This ensures that the component can be recovered later.
-            kwargs["seed"] = seed
-            self._update_seeds(model_key)
+            if seed:
+                kwargs["seed"] = seed
+                self._update_seeds(model_key)
             if vis_filter is not None:
                 kwargs["vis_filter"] = vis_filter
             self._components[model_key] = kwargs

hera_sim/tests/test_simulator.py
@@ -563,9 +563,10 @@ def test_none_seed(base_sim):
 
 
 def test_none_seed_state_recovery(base_sim):
-    base_sim.add("noiselike_eor", seed=None)
-    vis = base_sim.get("noiselike_eor")
-    assert np.allclose(base_sim.data.data_array, vis)
+    with pytest.warns(UserWarning, match="seed the random state"):
+        base_sim.add("noiselike_eor", seed=None)
+        vis = base_sim.get("noiselike_eor")
+    assert not np.allclose(base_sim.data.data_array, vis)
 
 
 @pytest.mark.parametrize("seed", [3.14, "redundant", "unsupported"])