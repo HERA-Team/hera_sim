fix: don't convert baseline units in pntsrc_fg
The baseline vector passed to the PointSourceForeground's call method
was being converted to nanoseconds prior to this update. This behavior
was inconsistent with the documentation, as well as other classes, which
use the convention that the baseline vector is already provided in units
of nanoseconds. This commit fixes that discrepancy.

hera_sim/foregrounds.py
@@ -255,8 +255,8 @@ class PointSourceForeground(Foreground):
             f0,
         ) = self._extract_kwarg_values(**kwargs)
 
-        # get baseline length in nanoseconds
-        bl_len_ns = np.linalg.norm(bl_vec) / constants.c.value * units.s.to("ns")
+        # get baseline length (it should already be in ns)
+        bl_len_ns = np.linalg.norm(bl_vec)
 
         # randomly generate source RAs
         ras = np.random.uniform(0, 2 * np.pi, nsrcs)