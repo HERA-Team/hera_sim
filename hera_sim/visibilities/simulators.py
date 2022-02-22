7c39a1446f14b0b7b4b9a9be9d222022b6b33828
piyanat <piyanat.kittiwisit@gmail.com>
2022-02-22 20:45:59 +0200

    Add temporary fix for future array shape.
    pyuvdata switched to using `future_array_shapes` by default from
    `pyuvdata>=2.7.0`. Many functionality of `hera_sim=2.x.x` relies
    on the `current_array_shapes`. The full fixes are planned for
    `hera_sim=3.0.0`.

--- hera_sim/visibilities/simulators.py
+++ hera_sim/visibilities/simulators.py
@@ -88,10 +88,16 @@ class ModelData:
 
     def _process_uvdata(self, uvdata: UVData | str | Path):
         if isinstance(uvdata, UVData):
+            # Temporary fix for future array shape - to be removed after v3.
+            if uvdata.future_array_shapes:
+                uvdata.use_current_array_shapes()
             return uvdata
         elif isinstance(uvdata, (str, Path)):
             out = UVData()
             out.read(str(uvdata))
+            # Temporary fix for future array shape - to be removed after v3.
+            if out.future_array_shapes:
+                out.use_current_array_shapes()
             return out
         else:
             raise TypeError(
